b85870ed3a08c9ba84ad2a100e4010db6fe42f77
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2025-09-03 10:49:23 +0530

    Completes ruby_basics/9_debugging exercise

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 # First, we're going to practice reading the Stack Trace
 # Don't look at this method prior to running the test
 # Type 'rspec' into the terminal to run Rspec
@@ -10,7 +12,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -24,7 +26,7 @@ def increment_greatest_value(nested_array)
   greatest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value
         greatest_value = current_value
       end
     end
@@ -37,8 +39,11 @@ end
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split
+  string_array = string.downcase.split("")
+  # p string_array
   unique_length = string_array.uniq.length
+  # puts unique_length
+  # puts original_length
   original_length == unique_length
 end
 
@@ -50,7 +55,8 @@ end
 # Once you find the error, fix it and get the test to pass
 
 def yell_greeting(string)
+  # binding.pry
   name = string
-  name = name.downcase
+  name = name.upcase
   greeting = "WASSAP, #{name}!"
 end
